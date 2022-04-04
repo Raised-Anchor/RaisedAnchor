83cd7e37f7bc6635abf2883b191fa1324d7aecbf
Jerry Robertson <jerry.r.robertson1@gmail.com>
2022-04-04 13:15:12 -0500

    Had to build to fix. Also removed anchor tags from names until bio pages are completed

--- src/component/ContactCard.js
+++ src/component/ContactCard.js
@@ -3,7 +3,9 @@ import { SiGmail } from "react-icons/si";
 export const ContactCard = ({ contact }) => {
   return (
     <>
-      <a>{contact.name}</a> -{" "}
+      {/* <a> */}
+      {contact.name}
+      {/* </a> */}-{" "}
       <a href={"mailto:" + contact.email}>
         <SiGmail />
       </a>
